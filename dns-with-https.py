09bfc99132053b37c346e1618fb9a0c6814b5b39
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-09-01 23:07:19 -0400

    Update dns-with-https.py

--- dns-with-https.py
+++ dns-with-https.py
@@ -2,7 +2,7 @@ import requests, uuid
 
 url = raw_input("Enter URL (ex. google.com): ")
 rand = str(uuid.uuid4())
-response = requests.get('https://dns.google.com/resolve?name=' + url + '&random_padding=' + rand)
+ = requests.get('https://dns.google.com/resolve?name=' + url + '&random_padding=' + rand)
 data = response.json()
 ip = data['Answer'][0]['data']
 cd = data['CD']
